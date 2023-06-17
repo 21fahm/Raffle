fa7d7d131fd8662cd4f9ec16aaed0f4fa9624b5f
Shawn Kimtai <shawnkimtai@gmail.com>
2023-06-17 22:38:45 +0300

    small changes

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -5,7 +5,7 @@ const BASE_FEE = ethers.utils.parseEther("0.25");
 const GAS_PRICE_LINK = 1e9;
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
-  const { deploy, log, get } = deployments;
+  const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const args = [BASE_FEE, GAS_PRICE_LINK];
 
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -8,7 +8,7 @@ const networkConfig = {
     gasLane:
       "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
     callbackGasLimit: "500000",
-    //add subId here from chainlink.
+    //add subId here
     interval: "30",
   },
   1: {
